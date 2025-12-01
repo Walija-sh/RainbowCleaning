bec4e84e1d7c2c4f670ab90a83dd4747acd90aaf
Walija-sh <walijash@gmail.com>
2025-12-01 16:12:40 +0500

    added overview

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Cta from '../components/Cta'
 import OurExperience from '../components/OurExperience'
 import Reviews from '../components/Reviews'
+import Overview from '../components/Overview'
 
 const Home = () => {
   return (
     <div>
+      <Overview/>
       <Reviews/>
       <OurExperience/>
       <div className="h-[100px] bg-white w-full"></div>
